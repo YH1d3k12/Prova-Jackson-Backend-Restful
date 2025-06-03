6d09cd1c7e1beaae2539153f3b947c96b8e37c34
Djalma <djalmahy@hotmail.com>
2025-06-02 22:06:11 -0300

    Feat: created swagger base documentation.

--- middleend/src/app.js
+++ middleend/src/app.js
@@ -5,9 +5,11 @@ const CategoryRouter = require('./routes/category.js');
 const ProductRouter = require('./routes/product.js');
 const OrderRouter = require('./routes/order.js');
 const OrderItemsRouter = require('./routes/order-item.js');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const swaggerDocument = require('./docs/swagger.json');
 
 app.use(express.json());
 app.use(cors());
@@ -17,7 +19,9 @@ app.use('/api/category', CategoryRouter);
 app.use('/api/product', ProductRouter);
 app.use('/api/order', OrderRouter);
 app.use('/api/order/:orderId/item', OrderItemsRouter);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`API Documentation is available at http://localhost:${PORT}/api/docs`);
 });
